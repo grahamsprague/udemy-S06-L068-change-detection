99ecdeebf768fffbb11e18a8dd779c439bbdca59
Graham Sprague <graham.sprague@boscww-nmx2026.fios-router.home>
2018-03-20 20:29:07 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses = [
+    { id: 0, name: 'course 1' },
+    { id: 1, name: 'course 2' },
+    { id: 2, name: 'course 3' },
+  ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onChange(course) {
+    course.name = 'changed';
+  }
 }
